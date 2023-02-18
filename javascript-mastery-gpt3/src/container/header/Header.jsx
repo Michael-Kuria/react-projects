5bd69c52770a874720213592b439e359f9f7d77a
michael <mchege78@gmail.com>
2023-02-18 15:07:11 +0300

    implemented header

--- javascript-mastery-gpt3/src/container/header/Header.jsx
+++ javascript-mastery-gpt3/src/container/header/Header.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 import "./header.css";
+import ai from "../../assets/ai.png";
+import people from "../../assets/people.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="gpt3__header section__padding" id="header">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3__header-input">
+          <input type="email" placeholder="Your Email Address" />
+          <button>Get Started</button>
+        </div>
+        <div className="gpt3__header-people">
+          <img src={people} />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} />
+      </div>
+    </header>
+  );
 };
 export default Header;
