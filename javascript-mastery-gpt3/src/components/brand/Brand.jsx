11f6f90c4fc0c53eacbe844975f1c3cee061c7b0
michael <mchege78@gmail.com>
2023-02-19 20:33:30 +0300

    added brand

--- javascript-mastery-gpt3/src/components/brand/Brand.jsx
+++ javascript-mastery-gpt3/src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <section className="gpt3__brand section__padding">
+      <div>
+        <img src={google} />
+      </div>
+      <div>
+        <img src={slack} />
+      </div>
+      <div>
+        <img src={atlassian} />
+      </div>
+      <div>
+        <img src={dropbox} />
+      </div>
+      <div>
+        <img src={shopify} />
+      </div>
+    </section>
+  );
 };
 
 export default Brand;
