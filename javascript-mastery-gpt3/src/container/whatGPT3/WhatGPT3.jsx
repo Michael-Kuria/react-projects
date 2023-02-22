933bf6422eacf9e59986ab78186e4b343d973da2
michael <mchege78@gmail.com>
2023-02-22 10:24:08 +0300

    added feature

--- javascript-mastery-gpt3/src/container/whatGPT3/WhatGPT3.jsx
+++ javascript-mastery-gpt3/src/container/whatGPT3/WhatGPT3.jsx
@@ -2,7 +2,49 @@ import React from "react";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <section className="section__padding">
+      <div className="gpt3__what section__margin">
+        <div className="gpt3__what--top">
+          <h3>What is GPT-3</h3>
+          <p>
+            We so opinion friends me message as delight. Whole front do of plate
+            heard oh ought. His defective nor convinced residence own.
+            Connection has put impossible own apartments boisterous. At jointure
+            ladyship an insisted so humanity he. Friendly bachelor entrance to
+            on by.
+          </p>
+        </div>
+        <div className="gpt3__what--middle">
+          <h2>The possibilities are beyond your imagination</h2>
+          <p>Explore the Library</p>
+        </div>
+        <div className="gpt3__what--bottom">
+          <div className="gpt3__what-library">
+            <h3>Chatbots</h3>
+            <p>
+              We so opinion friends me message as delight. Whole front do of
+              plate heard oh ought.
+            </p>
+          </div>
+          <div className="gpt3__what-library">
+            <h3>Knowledgebase</h3>
+            <p>
+              At jointure ladyship an insisted so humanity he. Friendly bachelor
+              entrance to on by. As put impossible own apartments b
+            </p>
+          </div>
+          <div className="gpt3__what-library">
+            <h3>Education</h3>
+            <p>
+              At jointure ladyship an insisted so humanity he. Friendly bachelor
+              entrance to on by. As put impossible own apartments b
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default WhatGPT3;
