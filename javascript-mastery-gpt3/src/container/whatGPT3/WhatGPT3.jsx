1d28da9549f62cb84c83f5e3c486acf116c4dfec
michael <mchege78@gmail.com>
2023-02-22 11:23:30 +0300

    added whatGPT3

--- javascript-mastery-gpt3/src/container/whatGPT3/WhatGPT3.jsx
+++ javascript-mastery-gpt3/src/container/whatGPT3/WhatGPT3.jsx
@@ -1,47 +1,39 @@
 import React from "react";
+import Feature from "../../components/feature/Feature";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <section className="section__padding">
-      <div className="gpt3__what section__margin">
-        <div className="gpt3__what--top">
-          <h3>What is GPT-3</h3>
-          <p>
-            We so opinion friends me message as delight. Whole front do of plate
+    <section className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature
+          title="What is GPT-3"
+          text="We so opinion friends me message as delight. Whole front do of plate
             heard oh ought. His defective nor convinced residence own.
             Connection has put impossible own apartments boisterous. At jointure
             ladyship an insisted so humanity he. Friendly bachelor entrance to
-            on by.
-          </p>
-        </div>
-        <div className="gpt3__what--middle">
-          <h2>The possibilities are beyond your imagination</h2>
-          <p>Explore the Library</p>
-        </div>
-        <div className="gpt3__what--bottom">
-          <div className="gpt3__what-library">
-            <h3>Chatbots</h3>
-            <p>
-              We so opinion friends me message as delight. Whole front do of
-              plate heard oh ought.
-            </p>
-          </div>
-          <div className="gpt3__what-library">
-            <h3>Knowledgebase</h3>
-            <p>
-              At jointure ladyship an insisted so humanity he. Friendly bachelor
-              entrance to on by. As put impossible own apartments b
-            </p>
-          </div>
-          <div className="gpt3__what-library">
-            <h3>Education</h3>
-            <p>
-              At jointure ladyship an insisted so humanity he. Friendly bachelor
-              entrance to on by. As put impossible own apartments b
-            </p>
-          </div>
-        </div>
+            on by."
+        />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore the Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-features">
+        <Feature
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+        />
+        <Feature
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
+        <Feature
+          title="Education"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
       </div>
     </section>
   );
