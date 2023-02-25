c9477d9b5d8b97d3f9382ca4ab3ff2aa0a719f0e
michael <mchege78@gmail.com>
2023-02-25 14:57:41 +0300

    added cta

--- javascript-mastery-gpt3/src/components/cta/CTA.jsx
+++ javascript-mastery-gpt3/src/components/cta/CTA.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./cta.css";
 
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="gt3__cta section__margin">
+      <div className="gt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h4>Register today & start exploring the endless possiblities.</h4>
+      </div>
+      <button type="button">Get Started</button>
+    </div>
+  );
 };
 
 export default CTA;
