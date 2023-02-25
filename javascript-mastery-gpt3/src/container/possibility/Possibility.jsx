4b3e3f707f52761fb98da0753afec9283bce6d4c
michael <mchege78@gmail.com>
2023-02-25 13:53:23 +0300

    added possibility

--- javascript-mastery-gpt3/src/container/possibility/Possibility.jsx
+++ javascript-mastery-gpt3/src/container/possibility/Possibility.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./possibility.css";
+import posibility from "../../assets/possibility.png";
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <section className="gpt3__posibility section__padding" id="posibility">
+      <div className="gpt3__posibility-image">
+        <img src={posibility} />
+      </div>
+      <div className="gpt3__posibility-desc">
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4>Request Early Access to Get Started</h4>
+      </div>
+    </section>
+  );
 };
 
 export default Possibility;
