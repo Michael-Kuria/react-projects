33db39bc168eab23e4d727d804b45b0288a75abc
michael <mchege78@gmail.com>
2023-02-25 17:18:44 +0300

    added blog and article

--- javascript-mastery-gpt3/src/components/article/Article.jsx
+++ javascript-mastery-gpt3/src/components/article/Article.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ image, date, title }) => {
+  return (
+    <div className="gpt3__article">
+      <div className="gpt3__article-image">
+        <img src={image} />
+      </div>
+      <div className="gpt3__article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+
+        <p>Read more</p>
+      </div>
+    </div>
+  );
 };
 export default Article;
--- javascript-mastery-gpt3/src/container/blog/Blog.jsx
+++ javascript-mastery-gpt3/src/container/blog/Blog.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { Article } from "../../components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports";
 import "./blog.css";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <section className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-main">
+        <Article
+          image={blog01}
+          date="Sep 26, 2021"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+      </div>
+      <div className="gpt3__blog-next">
+        <Article
+          image={blog02}
+          date="Sep 26, 2021"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          image={blog03}
+          date="Sep 26, 2021"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          image={blog04}
+          date="Sep 26, 2021"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          image={blog05}
+          date="Sep 26, 2021"
+          title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
