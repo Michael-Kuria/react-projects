ef468ce8f9c419b3e960c00d11f9e6dc19784c36
michael <mchege78@gmail.com>
2023-08-09 12:25:37 +0300

    updated team profile to include responsiveness

--- tailwind-css-projects/src/components/TeamProfile.jsx
+++ tailwind-css-projects/src/components/TeamProfile.jsx
@@ -2,33 +2,33 @@ const TeamList = [
   {
     image:
       "https://images.pexels.com/photos/3394347/pexels-photo-3394347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    name: "Alex Martif",
+    name: "Martin Martif",
+    designation: "Marketing Agent",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=150",
+    name: "John Martif",
     designation: "Marketing Agent",
   },
   {
     image:
       "https://images.pexels.com/photos/3394347/pexels-photo-3394347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    name: "Alex Martif",
-    designation: "Marketing Agent",
-  },
-  {
-    image:
-      "https://images.pexels.com/photos/3394347/pexels-photo-3394347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    name: "Alex Martif",
+    name: "Jane Martif",
     designation: "Marketing Agent",
   },
 ];
 
-const Profile = ({ image, name, designation }) => {
+const ProfileCard = ({ image, name, designation }) => {
   return (
-    <div className="h-72 w-64">
-      <div className="h-full bg-emerald-600 rounded flex flex-col justify-center items-center hover:bg-emerald-400">
+    <div className="h-60 w-48 bg-emerald-600 mb-4 rounded md:ml-4 lg:h-72 lg:w-60 lg:ml-8">
+      <div className="p-4 flex flex-col items-center">
         <img
-          className="rounded-full mb-4 h-48 w-48 object-cover object-left-top"
+          className="rounded-full h-40 w-40 object-cover object-left-top mb-2 lg:h-52 lg:w-52"
           src={image}
-          alt=""
+          alt={`${name} image`}
         />
-        <h3 className="text-gray-100 text-lg">{name}</h3>
+        <p className="text-gray-100 text-md lg:text-lg">{name}</p>
         <p className="text-sm">{designation}</p>
       </div>
     </div>
@@ -37,10 +37,10 @@ const Profile = ({ image, name, designation }) => {
 
 const TeamProfile = () => {
   return (
-    <div className="h-full w-full bg-gray-200 flex items-center">
-      <div className="flex  w-full ml-8 mr-8 gap-6  justify-center">
-        {TeamList.map((profile, index) => (
-          <Profile key={index} {...profile} />
+    <div className="h-full w-full flex justify-center">
+      <div className="flex flex-col mt-10 md:flex-row">
+        {TeamList.map((person, index) => (
+          <ProfileCard key={index} {...person} />
         ))}
       </div>
     </div>
